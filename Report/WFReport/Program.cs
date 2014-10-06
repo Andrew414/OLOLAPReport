7c5ce6d3c83f4021896890d8b408a1a314530bbe
Andrey Fedchuk <andrew414414@gmail.com>
2014-10-06 20:37:46 +0300

    *Singleton* created. Now only one instance of the app can be running at the one moment.

--- Report/WFReport/Program.cs
+++ Report/WFReport/Program.cs
@@ -14,9 +14,31 @@ namespace WFReport
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, "{8D719F92_EA4B_41F1_9905_0C914E45414C_Ololap");
+            try
+            {
+                if (mutex.WaitOne(0, false))
+                {
+                    // Run the application
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    MessageBox.Show("Another instance of the application is already running.\nPlease close all the other instances if you want to open the new one",
+                        "Application cannot be run", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            finally
+            {
+                if (mutex != null)
+                {
+                    mutex.Close();
+                    mutex = null;
+                }
+            }
+
         }
     }
 }
